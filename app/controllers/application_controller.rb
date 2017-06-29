106454e050584d9ae1133178ba593323fe3a4c97
github username <github email address>
2017-06-29 13:40:25 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 require_relative '../../config/environment'
+require "pry"
 
 class ApplicationController < Sinatra::Base
 
@@ -7,7 +8,49 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/' do
   end
+
+  get '/posts/new' do
+    erb :new
+  end
+
+  post '/posts' do
+    @new_post = Post.new(name: params[:name], content: params[:content])
+    @new_post.save
+    erb :index
+  end
+
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id].to_i)
+    @post.destroy
+    erb :delete 
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id].to_i)
+    @post.update(params[:post])
+    redirect "/posts/#{params[:id]}"
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id].to_i)
+    erb :show
+  end
+
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id].to_i)
+    erb :edit
+  end
+
+
+
+
+
 end
